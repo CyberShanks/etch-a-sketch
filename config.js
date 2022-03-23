eebdb4b4e0256d096ef1e5565d349ff11466ad6d
Shashank <shashankmish27@gmail.com>
2022-03-23 11:09:49 +0530

    using EventListener, Fix Box Creation Logic

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
-//displays no. of boxes selected by slider
-function outputUpdate(vol) {
-    document.querySelector('#num-box').value = vol;
+//add event listener to slider
+let sliderNode = document.querySelector('#slider')
+sliderNode = document.addEventListener('input', outputUpdate);
+
+
+
+
+
+function outputUpdate(e) {
+    //
+    var value = document.getElementById("slider").value;
+    console.log(value);
+    outputNode = document.querySelector('#num-box');
+    outputNode.textContent = value;
     //slider value is buggy. if you move slider up and down then more and more boxes keep on being generated. Also, initially no boxes are generated.
 /*OnChange vs OnInput... I think OnChange needs to be used, and the number of boxes that need to be generated should come from how many the user has selected through the slider.*/
     
@@ -11,7 +22,8 @@ function outputUpdate(vol) {
     box = document.createElement('div');
     //add class to it
     box.classList.add('box');
-    for (let index = 0; index < vol; index++) {
+    //get current number of boxes, and then compare that with the value on the slider, and then add or subtract boxes accordingly
+    for (let index = 0; index < value; index++) {
         container.appendChild(box);
     }
 }
