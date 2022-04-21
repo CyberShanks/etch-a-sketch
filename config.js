c2a99f206fdf379ae86cfde3346a220cc654bf71
Shashank <shashankmish27@gmail.com>
2022-04-22 00:49:57 +0530

    Box Size adjustment logic complete

--- config.js
+++ config.js
@@ -1,27 +1,29 @@
 //add event listener to slider
 let sliderNode = document.querySelector('#slider')
-sliderNode = document.addEventListener('input', outputUpdate);
+
+
+sliderNode.addEventListener('input', outputUpdate);
+
+sliderNode.addEventListener('change', displayBox);
 
 function outputUpdate(e) {
     var value = document.getElementById("slider").value; //slider value
-    console.log("function value -> " + value);
     outputNode = document.querySelector('#num-box');
     outputNode.textContent = value;
-
-    //call function to generate square number of boxes
-    displayBox(value**2);
-
 }
 
-function displayBox(boxValueReq) {
-    //selects the main box. INITIALIZE THE DEFAULT HERE
+function displayBox(e) {
+    var value = document.getElementById("slider").value;
+    var boxValueReq = value ** 2;
+    var boxDimension = 500/value;
+
+    //selects the main box
     grid = document.querySelector('#main-grid');
-
     childCount = grid.childElementCount
-
+    console.log(childCount);
     if (boxValueReq > childCount) {
-        //add number of boxes required which is the difference between current and required
-        for (; childCount <= boxValueReq; childCount++) {
+        //add number of boxes required which is the difference between current and required. FAULTY LOGIC, IF YOU INCREASE SLIDER AND THEN DECREASE, IT WILL RESULT IN N-1 BOXES
+        for (; childCount < boxValueReq; childCount++) {
             box = document.createElement('div');
             box.classList.add('box');
             grid.appendChild(box);
@@ -29,20 +31,24 @@ function displayBox(boxValueReq) {
     }
     
     else if (boxValueReq < childCount) {
-        //this might not work because box is a newnode, not a childnode...
-        for (; childCount >= boxValueReq; childCount--) {
+        for (; childCount > boxValueReq; childCount--) {
             removeBox = document.querySelector('.box');
             grid.removeChild(removeBox);
         }
     }
+    //adjust box sizes after all the boxes have been added or subtracted
+    boxClass = document.querySelectorAll('.box');
+    boxClass.forEach(box => {
+        box.style['width'] = boxDimension + "px";
+        box.style['height'] = boxDimension + "px";
+    });
+
 }
 
 //initializes initial number of boxes in grid
 window.addEventListener('load', (e) => {
     var value = document.getElementById("slider").value;
-    console.log("loaded, value = " + value);
     const grid = document.querySelector('#main-grid');
-    console.log("child elements = " + grid.childElementCount);
 
     for (let counter = 0; counter < (value**2); counter++) {
         var box = document.createElement('div');
